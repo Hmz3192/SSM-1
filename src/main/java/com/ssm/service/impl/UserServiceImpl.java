f2b8dd5943e60b24555a0a2f6217e046ebffa2b5
Hmz <1134598796@qq.com>
2017-06-16 19:39:02 +0800

    SSm-1

---
+++ src/main/java/com/ssm/service/impl/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.ssm.service.impl;
+
+
+import com.ssm.dao.UserDao;
+import com.ssm.model.User;
+import com.ssm.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by Zhangxq on 2016/7/15.
+ */
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class UserServiceImpl implements UserService {
+    
+    @Resource
+    private UserDao userDao;
+
+    public User getUserById(Long userId) {
+        return userDao.selectUserById(userId);
+    }
+    
+    public User getUserByPhoneOrEmail(String emailOrPhone, Short state) {
+        return userDao.selectUserByPhoneOrEmail(emailOrPhone,state);
+    }
+    
+    public List<User> getAllUser() {
+        return userDao.selectAllUser();
+    }
+}
